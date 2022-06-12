ca05d4d88e0cbcd26ad54f9e0d23e20bce7934b2
Chloekkk <skdusdl8804@gmail.com>
2022-06-12 15:27:21 +0900

    feat: Update minishell and token headers

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 18:36:42 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/04 18:49:36 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/12 14:22:13 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,22 @@
 #include "../libft/libft.h"
 #include "ast.h"
 
+# define NULL_LINE 0;
+# define ERROR_FLAG -1
+# define SUCCESS_FLAG 0
+
 typedef struct s_manager
 {
   t_list **envp;
-  t_list **commands;
+  char *command_line;
+  int command_len;
+  int rc; // read counter
 } t_manager;
+
+extern t_manager manager;
+
+/* UTILS */
+int	ft_isspace(int c);
+
+/* PARSER */
+char		*get_token();
--- include/token.h
+++ include/token.h
@@ -6,25 +6,36 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 14:40:53 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/04 16:33:35 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/12 11:44:43 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# define T_NULL 0
-# define T_WORD 1 /* WORD -> word | "word" | 'word' */
-# define T_PIPE 2
-# define T_REDIRECT 3
-# define T_DOUBLE_QUOTES 4
-# define T_SINGLE_QUOTES 5
-typedef struct s_token {
-	int	type;
-	char	*str;
-}	t_token;
+// # define T_NULL 0
+// # define T_WORD 1 /* WORD -> word | "word" | 'word' */
+// # define T_PIPE 2
+// # define T_REDIRECT 3
+// # define T_DOUBLE_QUOTES "\""
+// # define T_SINGLE_QUOTES 5
 
-/* TODO: 이 부분 다시 생각해보기 */
+typedef enum e_token {
+	T_NULL = 0,
+	T_WORD = 1,
+	T_IN = '<',
+	T_OUT = '>',
+	T_PIPE = '|',
+	T_DOUBLE_QUOTES = '\"',
+	T_SINGLE_QUOTES = '\''
+} t_token;
+
+// typedef struct s_token {
+// 	int	type;
+// 	char	*str;
+// }	t_token;
+
+// /* TODO: 이 부분 다시 생각해보기 */
 typedef enum e_redirect_op {
-	IN,
-	OUT,
+	IN = '<',
+	OUT = '\'',
 	HEREDOC,
 	APPEND
 } t_redirect_op;
