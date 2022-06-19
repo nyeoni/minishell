a85444de47609a51bfa6523fc42520f38717a93b
Chloekkk <skdusdl8804@gmail.com>
2022-06-19 23:46:01 +0900

    refactor: Refactor lexical_analyzer return type to t_token

--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -6,79 +6,101 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 14:11:59 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/12 15:25:23 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/19 23:45:28 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		get_word(char **begin, char **end)
+void	get_word(t_token *token, char **begin, char **end)
 {
-	while (**end && (!ft_isspace(**end) || ft_strchr("<>|", **end)))
-		(*end)++;
-	return (SUCCESS_FLAG);
+	token->type = T_WORD;
+	while (**end && (!ft_isspace(**end)))
+	{
+		if (ft_strchr("<>|\"'", *(*end + 1)))
+			return ;
+		else
+			(*end)++;
+	}
 }
 
-int		get_quote(char **begin, char **end)
+void	get_quote(t_token *token, char **begin, char **end)
 {
+	token->type = T_WORD;
 	*end = ft_strchr(*begin + 1, **begin);
 	if (!*end)
 	{
 		printf("인용문이 닫히지 않았습니다!\n");
-		return (ERROR_FLAG);
+		// 여기서 입력 계속 받는거 처리
+		return ;
 	}
 	else
-	{
 		(*end)++;
-		return (SUCCESS_FLAG);
+}
+
+void	get_pipe(t_token *token, char **begin, char **end)
+{
+	token->type = T_PIPE;
+	(*end)++;
+}
+
+void	get_redirect_op(t_token *token, char **begin, char **end)
+{
+	if (**begin == T_IN && *(*begin + 1) == T_IN)
+	{
+		token->type = T_HEREDOC;
+		*end += 2;
+	}
+	else if (**begin == T_OUT && *(*begin + 1) == T_OUT)
+	{
+		token->type = T_APPEND;
+		*end += 2;
+	}
+	else if (**begin == T_IN)
+	{
+		token->type = T_IN;
+		(*end)++;
+	}
+	else if (**begin == T_OUT)
+	{
+		token->type = T_OUT;
+		(*end)++;
 	}
 }
 
-int lexical_analysis(char **begin, char **end)
+void	lexical_analysis(t_token *token, char **begin, char **end)
 {
-	int flag;
-
-	flag = SUCCESS_FLAG;
-	*begin = &manager.command_line[manager.rc];
 	while (ft_isspace(manager.command_line[manager.rc]))
 		manager.rc++;
-
+	*begin = &manager.command_line[manager.rc];
 	*end = *begin;
 	if (**begin == T_DOUBLE_QUOTES || **begin == T_SINGLE_QUOTES)
-		flag = get_quote(begin, end);
-	else if (((**begin == T_IN && *(*begin + 1) == T_IN)
-			|| (**begin == T_OUT && *(*begin + 1) == T_OUT)))
-		*end += 2;
+		get_quote(token, begin, end);
 	else if (**begin == T_IN || **begin == T_OUT)
-		(*end)++;
+		get_redirect_op(token, begin, end);
 	else if (**begin == T_PIPE)
-		(*end)++;
+		get_pipe(token, begin, end);
 	else
-		flag = get_word(begin, end);
-	if (flag == SUCCESS_FLAG)
-		return (SUCCESS_FLAG);
-	else
-		return (ERROR_FLAG);
+		get_word(token, begin, end);
 }
 
-
-char	*get_token()
+t_token	get_token()
 {
 	char	*begin;
 	char	*end;
-	char	*token;
+	t_token	token;
 
+	token.type = T_NULL;
+	token.value = NULL;
+	printf("manager.rc: %d\n", manager.rc);
 	if (manager.rc >= manager.command_len)
-		return (0);
-	// 이 케이스는 인용문이 안닫혔을 경우 밖에 없음
-	if (lexical_analysis(&begin, &end))
-		return (0);
-	token = ft_calloc(end - begin + 1, sizeof(char));
-	if (!token)
-		return (0);
-	if (!ft_strlcpy(token, begin, end - begin + 1))
-		return (0);
-	manager.rc += end - begin + 1;
-
+		return (token); // EOF error
+	lexical_analysis(&token, &begin, &end);
+	token.value = ft_calloc(end - begin + 1, sizeof(char));
+	if (!token.value)
+		return (token); // malloc error
+	if (!ft_strlcpy(token.value, begin, end - begin + 1))
+		return (token); // copy error
+	manager.rc += end - begin;
 	return (token);
 }
