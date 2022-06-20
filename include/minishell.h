0e8116b0336ea2e0428312ce46636dbcaaef110f
Chloekkk <skdusdl8804@gmail.com>
2022-06-20 14:11:27 +0900

    feat: Add parse funtions to minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 18:36:42 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/20 00:44:35 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/20 05:00:41 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,8 @@
 typedef struct s_manager
 {
   t_list **envp;
-  char *command_line;
-  int command_len;
-  int rc; // read counter
+  char *command_line; // 이건 나중에 처리할 수 있는지 고민해보기
+  int rc; // read counter 이것도 나중에 static 처리하기
 } t_manager;
 
 typedef enum e_fetch_type
@@ -40,4 +39,12 @@ extern t_manager manager;
 int	ft_isspace(int c);
 
 /* PARSER */
-t_token get_token();
+t_token	fetch_token(t_fetch_type type);
+void	lexical_analyzer(t_token *token, char **begin, char **end);
+t_ast	*syntax_analyzer();
+char	*match(t_type type);
+void	syntax_pipe_line(t_ast *ast_pipe_line);
+void	syntax_command(t_ast *ast_command);
+void	syntax_simple_command(t_simple_command *simple_command);
+void	syntax_redirects(t_ast *ast_redirects);
+void	syntax_io_redirect(t_io_redirect *io_redirect);
