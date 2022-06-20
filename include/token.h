1ec5cd61b25af4be2bc686dbf187507f12246b8d
Chloekkk <skdusdl8804@gmail.com>
2022-06-20 14:11:05 +0900

    refactor: Refactor token.h

--- include/token.h
+++ include/token.h
@@ -6,28 +6,15 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 14:40:53 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/19 18:35:50 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/20 14:10:44 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// # define T_NULL 0
-// # define T_WORD 1 /* WORD -> word | "word" | 'word' */
-// # define T_PIPE 2
-// # define T_REDIRECT 3
-// # define T_DOUBLE_QUOTES "\""
-// # define T_SINGLE_QUOTES 5
-typedef char *WORD;
-
 typedef enum e_type {
-	T_NULL = 0,
-	T_WORD = 1,
-	T_HEREDOC = 2,
-	T_APPEND = 3,
-	T_IN = '<',
-	T_OUT = '>',
-	T_PIPE = '|',
-	T_DOUBLE_QUOTES = '\"',
-	T_SINGLE_QUOTES = '\''
+	T_NULL,
+	T_WORD,
+	T_REDIRECT,
+	T_PIPE
 } t_type;
 
 typedef struct s_token {
@@ -35,11 +22,17 @@ typedef struct s_token {
 	char	*value;
 }	t_token;
 
-// /* TODO: 이 부분 다시 생각해보기 */
+typedef enum e_unit {
+	U_IN = '<',
+	U_OUT = '>',
+	U_PIPE = '|',
+	U_DOUBLE_QUOTES = '\"',
+	U_SINGLE_QUOTES = '\''
+} t_unit;
+
 typedef enum e_redirect_op {
-	IN = '<',
-	OUT = '\'',
+	IN,
+	OUT,
 	HEREDOC,
 	APPEND
 } t_redirect_op;
-typedef t_redirect_op REDIRECT_OP;
