9cd6bd678606ddbc2871efd18fa2a66bec2341ec
Chloekkk <skdusdl8804@gmail.com>
2022-06-21 15:35:08 +0900

    chore: Refactor syntax_* functions strucutre

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hannkim <hannkim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 18:36:42 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/20 19:00:22 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/06/21 15:42:27 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,11 +56,11 @@ t_token				fetch_token(t_fetch_type type);
 void				lexical_analyzer(t_token *token, char **begin, char **end);
 t_ast				*syntax_analyzer(void);
 char				*match(t_type type);
-void				syntax_pipe_line(t_ast *ast_pipe_line);
-void				syntax_command(t_ast *ast_command);
-void				syntax_simple_command(t_simple_command *simple_command);
-void				syntax_redirects(t_ast *ast_redirects);
-void				syntax_io_redirect(t_io_redirect *io_redirect);
+void				syntax_pipe_line(t_ast **ast_pipe_line);
+void				syntax_command(t_ast **ast_command);
+void				syntax_redirects(t_ast **ast_redirects);
+void				syntax_simple_command(t_simple_command **simple_command);
+void				syntax_io_redirect(t_io_redirect **io_redirect);
 
 /* BUILTIN */
 void				execute_cmd(char **argv);
