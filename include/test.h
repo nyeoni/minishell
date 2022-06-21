df61f0cfeeaa3d7f058dc802275a476611a83527
Chloekkk <skdusdl8804@gmail.com>
2022-06-21 15:34:21 +0900

    chore: Update test_ast Makefile header

--- include/test.h
+++ include/test.h
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   test.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hannkim <hannkim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:22:32 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/20 19:27:07 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/06/21 15:41:59 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_H
 # define TEST_H
+
+# include "minishell.h"
+
+void	test_token(void);
+void test_ast(t_ast *ast);
 
 void	test_token(void);
 void	test_builtin(void);
