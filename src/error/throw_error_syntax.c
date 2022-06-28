a575ba866826673cac05e2721a89a47a56bebf9c
Chloekkk <skdusdl8804@gmail.com>
2022-06-28 17:25:43 +0900

    fix: Fix ps name

--- src/error/throw_error_syntax.c
+++ src/error/throw_error_syntax.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 23:59:10 by hannkim           #+#    #+#             */
-/*   Updated: 2022/06/28 00:32:36 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/28 17:25:33 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 */
 int	throw_error_syntax(t_token token)
 {
-	ft_putstr_fd("딸기쉘: syntax error near unexpected token `", 2);
+	ft_putstr_fd("blackhole-shell: syntax error near unexpected token `", 2);
 	if (token.type == T_PIPE)
 		ft_putstr_fd(token.value, 2);
 	else
