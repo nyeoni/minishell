14a954df78e2c7821c4f9748cd69495a8b596e6f
Chloekkk <skdusdl8804@gmail.com>
2022-06-29 21:25:47 +0900

    feat: Update BACKUP_FD for stdin backup

--- include/redirect.h
+++ include/redirect.h
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 18:27:34 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/29 02:19:34 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/06/29 21:16:00 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REDIRECT_H
 # define REDIRECT_H
+
+# define BACKUP_FD 255
 
 # include <fcntl.h>
 # include <stdio.h>
@@ -20,7 +22,7 @@
 int	redirect_in_file(char *file_path);
 int	redirect_out_file(char *file_path);
 int	redirect_append_file(char *file_path);
-int	redirect_heredoc(char *end_text);
+int	redirect_heredoc(char *end_text, char *heredoc_path);
 int	backup_std_fd(int std_fd[3]);
 int	reset_std_fd(int std_fd[3]);
 
--- src/redirect/backup_std_fd.c
+++ src/redirect/backup_std_fd.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 18:29:57 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/28 20:17:17 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/29 21:24:36 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	backup_std_fd(int std_fd[3])
 {
-	std_fd[READ] = dup(STDIN_FILENO);
+	std_fd[READ] = dup2(STDIN_FILENO, BACKUP_FD);
 	std_fd[WRITE] = dup(STDOUT_FILENO);
 	std_fd[ERROR] = dup(STDERR_FILENO);
 	if (std_fd[READ] == -1 || std_fd[WRITE] == -1 || std_fd[ERROR] == -1)
