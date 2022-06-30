d49524f0388205683e6153911df93ffe13fbc25a
Chloekkk <skdusdl8804@gmail.com>
2022-06-30 18:56:43 +0900

    fix: Fix free_io_redirect unlink heredoc_path

--- src/parser/free_io_redirect.c
+++ src/parser/free_io_redirect.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:11:55 by nkim              #+#    #+#             */
-/*   Updated: 2022/06/30 17:00:04 by nkim             ###   ########.fr       */
+/*   Updated: 2022/06/30 18:56:04 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 void	free_io_redirect(t_io_redirect *io_redirect)
 {
 	if (io_redirect->redirect_op == R_HEREDOC)
-		if (unlink(io_redirect->file_path) == -1)
-			perror("hihihi");
+		unlink(io_redirect->file_path);
 	if (io_redirect->file_path)
 		free(io_redirect->file_path);
 	if (io_redirect->end_text)
