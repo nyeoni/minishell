daf185712d5da4fe153790aed8b5e64f5eaf3ad6
Kimhannah <skqkdldhf98@naver.com>
2022-07-03 16:25:01 +0900

    refactor: Modify STDOUT to STDERR

--- src/error/throw_error_syntax.c
+++ src/error/throw_error_syntax.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   throw_error_syntax.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 23:59:10 by hannkim           #+#    #+#             */
-/*   Updated: 2022/07/01 16:15:32 by nkim             ###   ########.fr       */
+/*   Updated: 2022/07/03 16:17:41 by hannkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	throw_error_syntax(t_token token)
 {
 	ft_putstr_fd("blackhole-shell: syntax error near unexpected token `", 2);
 	if (token.type == T_PIPE)
-		ft_putstr_fd(token.value, 2);
+		ft_putstr_fd(token.value, STDERR_FILENO);
 	else
-		ft_putstr_fd("newline", 2);
-	ft_putendl_fd("'", 2);
+		ft_putstr_fd("newline", STDERR_FILENO);
+	ft_putendl_fd("'", STDERR_FILENO);
 	g_manager.exit_code = EXIT_SYNTAXERR;
 	return (ERROR_FLAG);
 }
--- src/error/throw_error_usage.c
+++ src/error/throw_error_usage.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   throw_error_usage.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 19:58:57 by hannkim           #+#    #+#             */
-/*   Updated: 2022/07/01 16:15:40 by nkim             ###   ########.fr       */
+/*   Updated: 2022/07/03 16:18:22 by hannkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@
 */
 int	throw_error_usage(char *cmd, char *argv)
 {
-	ft_putstr_fd("blackhole-shell: ", STDOUT_FILENO);
-	ft_putstr_fd(cmd, STDOUT_FILENO);
-	ft_putstr_fd(": ", STDOUT_FILENO);
-	write(STDOUT_FILENO, argv, 2);
-	ft_putstr_fd(": invalid option\n", STDOUT_FILENO);
-	ft_putstr_fd(cmd, STDOUT_FILENO);
-	ft_putstr_fd(": usage: ", STDOUT_FILENO);
-	ft_putstr_fd(cmd, STDOUT_FILENO);
-	ft_putstr_fd("\n", STDOUT_FILENO);
+	ft_putstr_fd("blackhole-shell: ", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	write(STDERR_FILENO, argv, 2);
+	ft_putstr_fd(": invalid option\n", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(": usage: ", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
 	return (EXIT_FAILURE);
 }
